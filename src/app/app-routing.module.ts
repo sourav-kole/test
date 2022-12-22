bf7d1ec69d1cf156eca8e96d3516c249fa79e39c
Sourav Kole <sourav.kole87@gmail.com>
2022-12-22 12:55:35 +0530

    added module federation and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'order',
+    //@ts-expect-error
+    loadChildren: () => import('order/module').then(m => m.OrderModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
